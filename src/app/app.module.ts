1729f167513c53849639067815c2ba86b2d08e94
Alberto Pardo Díaz <albertopardo21690@gmail.com>
2019-08-27 17:44:59 +0200

    Estructura incial de la plataforma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
+import { APP_ROUTES } from './app.routes';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { HeaderComponent } from './shared/admin/header/header.component';
+import { SidebarComponent } from './shared/admin/sidebar/sidebar.component';
+import { FooterComponent } from './shared/admin/footer/footer.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { BreadcrumbsComponent } from './shared/admin/breadcrumbs/breadcrumbs.component';
+import { PagesComponent } from './pages/pages.component';
+import { InicioComponent } from './pages/home/inicio/inicio.component';
+import { HeaderFrontComponent } from './shared/header-front/header-front.component';
+import { SidebarFrontComponent } from './shared/sidebar-front/sidebar-front.component';
+import { ModalComponent } from './auth/modal/modal.component';
+import { RegistroComponent } from './auth/registro/registro.component';
+import { RecuperarComponent } from './auth/recuperar/recuperar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    BreadcrumbsComponent,
+    PagesComponent,
+    InicioComponent,
+    HeaderFrontComponent,
+    SidebarFrontComponent,
+    ModalComponent,
+    RegistroComponent,
+    RecuperarComponent
   ],
   imports: [
+    APP_ROUTES,
     BrowserModule
   ],
   providers: [],
---
+++ src/app/shared/sidebar-front/sidebar-front.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-front',
+  templateUrl: './sidebar-front.component.html',
+  styleUrls: ['./sidebar-front.component.css']
+})
+export class SidebarFrontComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
